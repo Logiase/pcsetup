180eebcbfdbbc2c1688396792113ebefa44e0465
Logiase <logiase.syx@gmail.com>
2023-02-03 18:05:46 +0800

    Update pcsetup.ps1

--- pcsetup.ps1
+++ pcsetup.ps1
@@ -1,32 +1,29 @@
-$VerbosePreference = "Continue"
+param (
+    [switch]$InstallOpenSSHServer = $false,
+    [switch]$InstallWSL = $false
+)
 
-function Install-ScoopApp {
-    param (
-        [string]$Package
-    )
-
-    Write-Verbose -Message "Preparing to install $Package"
-    if (!(scoop info $Package).Installed) {
-        Write-Verbose -Message "Installing $Package"
-        scoop install $Package
-    }
-    else {
-        Write-Verbose -Message "Package $Package already installed! Skipping..."
-    }
-}
+$InformationPreference = "Continue"
 
 function Enable-ScoopBucket {
     param (
-        [string]$Bucket
+        [string] $Bucket
     )
 
-    if (!($(scoop bucket list).Name -eq "$Bucket")) {
-        Write-Verbose -Message "Adding Bucket $Bucket to scoop..."
-        scoop bucket add $Bucket
-    }
-    else {
-        Write-Verbose -Message "Bucket $Bucket already added! Skipping..."
-    }
+    Write-Information "Enabling scoop bucket $Bucket"
+    scoop bucket add $Bucket
+}
+
+function Install-ScoopApps {
+    param (
+        [string[]] $Apps
+    )
+
+    Write-Information "Updating scoop..."
+    scoop update
+
+    Write-Information "Installing Apps $Apps..."
+    scoop install $Apps
 }
 
 function Install-WingetApp {
@@ -34,57 +31,89 @@ function Install-WingetApp {
         [string]$PackageID
     )
 
-    Write-Verbose -Message "Preparing to install $Package"
-    Write-Verbose -Message "Installing $PackageID"
+    Write-Information -Message "Installing Apps $PackageID"
     winget install --silent --id "$PackageID" --accept-source-agreements --accept-package-agreements
 }
 
+Write-Information "Starting PC setup script..."
+Write-Information "Setting PowerShell Execution Policy to RemoteSigned..."
 if ((Get-ExecutionPolicy -Scope CurrentUser) -notcontains "RemoteSigned") {
-    Write-Verbose -Message "Setting Execution Policy for Current User..."
     Start-Process -FilePath "PowerShell" -ArgumentList "Set-ExecutionPolicy", "RemoteSigned", "-Scope", "CurrentUser" -Verb RunAs -Wait
-    Write-Output "Restart/Re-Run script!!!"
+    Write-Output "Need to restart/re-run to refresh Execution Policy"
     Start-Sleep -Seconds 10
     Break
 }
+else {
+    Write-Information "Execution Policy alread set to RemoteSigned, skipping."
+}
 
-# Install Scoop
-if (!(Get-Command -Name "scoop" -CommandType Application -ErrorAction SilentlyContinue)) {
-    Write-Verbose -Message "Installing Scoop..."
+Write-Information "Installing OpenSSH..."
+if ((Get-WindowsCapability -Online -Name 'OpenSSH.Client*').State -ne "Installed") {
+    Write-Information "Installing OpenSSH.Client..."
+    $Name = (Get-WindowsCapability -Online -Name 'OpenSSH.Client*').Name
+    Add-WindowsCapability -Online -Name $Name
+}
+else {
+    Write-Information "OpenSSH.Client Already Installed, skipping."
+}
+if ($InstallOpenSSHServer) {
+    if ((Get-WindowsCapability -Online -Name 'OpenSSH.Server*').State -ne "Installed") {
+        Write-Information "Installing OpenSSH.Server..."
+        $Name = (Get-WindowsCapability -Online -Name 'OpenSSH.Server*').Name
+        Add-WindowsCapability -Online -Name $Name
+
+        Write-Information "Starting OpenSSH.Server..."
+        if (!(Get-Service -Name 'sshd' -ErrorAction)) {
+            Write-Information "Service sshd not found!!!"
+        }
+        else {
+            if ((Get-Service -Name 'sshd').Status -ne "Running") {
+                Write-Information "Starting Service sshd..."
+                Start-Service sshd
+            }
+            else {
+                Write-Information "Service sshd already started."
+            }
+
+            Set-Service -Name sshd -StartupType Automatic
+
+            Write-Information "Checking sshd firewall rule..."
+            if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
+                Write-Information "Firewall Rule 'OpenSSH-Server-In-TCP' does not exist, creating it..."
+                New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+            }
+            else {
+                Write-Information "Firewall rule 'OpenSSH-Server-In-TCP' has been created and exists."
+            }
+        }
+    }
+    else {
+        Write-Information "OpenSSH.Server Already Installed, skipping."
+    }
+}
+
+if (!(Get-Command -Name 'scoop' -ErrorAction SilentlyContinue)) {
+    Write-Information 'Installing scoop...'
     irm get.scoop.sh | iex
 }
 
-# Install Winget
-
-# Install OpenSSH.Client on Windows 10+
-@'
-if ((Get-WindowsCapability -Online -Name OpenSSH.Client*).State -ne "Installed) {
-    Add-WindowsCapability -Online -Name OpenSSH.Client*
-}
-'@ > "${Env:Temp}\openssh.ps1"
-Start-Process -FilePath "PowerShell" -ArgumentList "${Env:Temp}\openssh.ps1" -Verb RunAs -Wait -WindowStyle Hidden
-Remove-Item -Path "${Env:Temp}\openssh.ps1" -Force
-
-# refreshenv
-Install-ScoopApp -Package refreshenv
-
-# Configure git
-Install-ScoopApp -Package git
+Write-Information 'Installing refreshenv and git first...'
+Install-ScoopApps -Apps refreshenv, git
 Start-Sleep -Seconds 5
 refreshenv
 Start-Sleep -Seconds 5
+
+Write-Information "Configuring git..."
 if (!$(git config --global credential.helper) -eq "manager-core") {
-    git config --global credential.helper manager-core
+    git config --global credential.help manager-core
 }
-if (!($Env:GIT_SSH)) {
-    Write-Verbose -Message "Setting GIT_SSH User Environment Variable"
+if (!($env:GIT_SSH)) {
+    Write-Information "Setting GIT_SSH User Environment Variable."
     [System.Environment]::SetEnvironmentVariable('GIT_SSH', (Resolve-Path (scoop which ssh)), 'USER')
 }
-if ((Get-Service -Name ssh-agent).Status -ne "Running") {
-    Start-Process -FilePath "PowerShell" -ArgumentList "Set-Service", "ssh-agent", "-StartupType", "Manual" -Verb RunAs -Wait -WindowStyle Hidden 
-}
 
-# Configure Aria2 Download Manager
-Install-ScoopApp -Package aria2
+Write-Information "Using aria2 for scoop..."
+Install-ScoopApps -Apps aria2
 if (!$(scoop config aria2-enabled) -eq $true) {
     scoop config aria2-enabled true
 }
@@ -93,66 +122,11 @@ if (!$(scoop config aria2-warning-enabled) -eq $false) {
 }
 
 Enable-ScoopBucket -Bucket "extras"
-Enable-ScoopBucket -Bucket "nerd-fonts"
-
-# UNIX Tools
-Write-Verbose -Message "Removing curl Alias..."
-if (Get-Alias -Name curl -ErrorAction SilentlyContinue) {
-    Remove-Item Alias::curl
-}
-if (!($Env:TERM)) {
-    Write-Verbose -Message "Setting TERM User Environment Variable"
-    [System.Environment]::SetEnvironmentVariable("TERM", "xterm-256color", "USER")
-}
-
-$ScoopApps = @(
-    # "curl"
-    "busybox",
-    "cacert",
-    "sudo",
-    
-    "python",
-    "go",
-    # "nodejs-lts"
-    
-    # "ffmpeg",
-    # "obs-studio"
-    # "wingetui",
-    "neovim"
-)
-foreach ($item in $ScoopApps) {
-    Install-ScoopApp -Package "$item"
-}
-
-$WingetApps = @(
-    "Microsoft.DotNet.DesktopRuntime.3_1",
-    "Microsoft.DotNet.DesktopRuntime.5",
-    "Microsoft.DotNet.DesktopRuntime.6",
-    "Microsoft.DotNet.DesktopRuntime.7",
-    "Microsoft.WindowsTerminal",
-    "Microsoft.Edge",
-    "Microsoft.Teams",
-    "Microsoft.PowerToys",
-    "Microsoft.PowerShell",
-    
-    # "Google.Chrome",
-    
-    "SomePythonThings.WingetUIStore",
-    
-    "ZeroTier.ZeroTierOne",
-    "Fndroid.ClashForWindows"
-)
-foreach ($item in $WingetApps) {
-    Install-WingetApp -PackageID "$item"
-}
 
 winget install Microsoft.VisualStudioCode --override '/SILENT /mergetasks="!runcode,addcontextmenufiles,addcontextmenufolders"'
 
-$wslInstalled = Get-Command "wsl" -CommandType Application -ErrorAction Ignore
-if (!$wslInstalled) {
-    Write-Verbose -Message "Installing Windows SubSystems for Linux..."
-    Start-Process -FilePath "PowerShell" -ArgumentList "wsl", "--install", "--no-distribution" -Verb RunAs -Wait -WindowStyle Hidden
-}
-Install-ScoopApp -Package archwsl
+if ($InstallWSL) {
+    Write-Information "Installing WSL..."
+    wsl --install --no-distribution
 
-# install my powershell configurations
+}
